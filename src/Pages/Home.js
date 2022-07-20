c08a0ec41e8f4462ee1dc8951ae8e2614fcfbd17
Syed Bakhtawar Fahim <87495620+Syed-Bakhtawar-Fahim@users.noreply.github.com>
2022-07-20 08:41:12 -0700

    commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import { Box } from '@mui/material'
 import HeroBanner from '../Component/HeroBanner'
 import Exercise from '../Component/Exercise'
 import SearchExercise from '../Component/SearchExercise'
+import BodyPart from '../Component/BodyPart'
 
 const Home = () => {
+
+  const [bodyPart, setBodyPart] = useState('all')
+  const [exercises, setExercises] = useState([])
   return (
     <>
-        <Box>
-          <HeroBanner />
-          <SearchExercise />
-          <Exercise />
-        </Box>
+      <Box>
+        <HeroBanner />
+        <SearchExercise
+          setExercises={setExercises}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+
+        />
+        {/* <BodyPart /> */}
+        <Exercise
+        exercises = {exercises}
+          setExercises={setExercises}
+          bodyPart={bodyPart}
+          
+
+        />
+      </Box>
     </>
   )
 }
